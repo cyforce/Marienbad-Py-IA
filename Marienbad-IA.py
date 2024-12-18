2206404cf67aa4b2384e838c8d68cdf020caf098
Cyforce <celian.touzeau@outlook.com>
2024-12-18 14:46:07 +0100

    Add functions to create game board and initialize game

--- Marienbad-IA.py
+++ Marienbad-IA.py
@@ -2,3 +2,12 @@ def display_board_with_bare(l):
     for i in l:
         print("| "*i, end="\n")
 
+def creer_plateau(n):
+    plateau = []
+    for i in range(1, n+1, 2):
+        plateau.append(i)
+    return plateau
+
+def jeu(nbTas):
+    plateau = creer_plateau(nbTas)
+    
